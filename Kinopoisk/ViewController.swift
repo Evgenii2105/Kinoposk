94edd594641c7657f58017f941ff313abcc4e61e
Evgenii2105 <Fomev1@yandex.ru>
2025-06-07 12:36:47 +0300

    Initial Commit

---
+++ Kinopoisk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kinopoisk
+//
+//  Created by Евгений Фомичев on 07.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
